fa0bc82c20ac70cf9bc4a02b074d782a1a5c94d5
ARTICUS939 <articus_mib@mail.ru>
2018-10-05 17:45:04 +0300

    практика 2

--- PractiseForEx2/Program.cs
+++ PractiseForEx2/Program.cs
@@ -10,6 +10,16 @@ namespace PractiseForEx2
     {
         static void Main(string[] args)
         {
+                string str = Console.ReadLine();
+                const string a_lett = "Aa";
+                int cnt = 0;
+                for (int i = 0; i < str.Length; i++)
+                    for (int j = 0; j < a_lett.Length; j++)     //на случай, если введена заглавная
+                        if (str[i] == a_lett[j])
+                            ++cnt;
+                Console.Write(cnt);
+                Console.ReadLine();
+            
         }
     }
 }
